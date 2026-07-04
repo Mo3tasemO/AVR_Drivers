466589e7f3d0dcde012e80f7bc2ecfdb17a93919
Moatasem <MoatasemElsharkawy12@gmail.com>
2026-07-04 20:22:36 +0300

    Update Timer 3 Driver

--- PIC18F4620_Drivers/MCAL_LAYER/Timer3/HAL_TIMER3.c
+++ PIC18F4620_Drivers/MCAL_LAYER/Timer3/HAL_TIMER3.c
@@ -1,1 +1,140 @@
+/* 
+ * File:   HAL_TIMER3.c
+ * Author: Moata
+ *
+ * Created on June 29, 2026, 9:14 PM
+ */
 
+#include "HAL_TIMER3.h"
+
+
+static inline void TMR3_MODE_SELECT(const TIMER3_T *_Timer);
+static inline void TMR3_RW_CFG_SELECT(const TIMER3_T *_Timer);
+#ifdef TIMER3_INTERRUPT_FEATURE_ENABLE
+    static void (*TIMER3_INTERRUPT_HANDLER)(void) = NULL;
+#endif
+static uint16 timer3_preload = 0;
+
+
+
+
+Std_ReturnType TIMER3_INIT(const TIMER3_T *_Timer){
+    Std_ReturnType ret = E_OK;
+    if(NULL == _Timer){
+        ret = E_NOT_OK;
+    }
+    else{
+        TIMER3_DISABLE();
+        TIMER3_PRESCALLER_SELECT(_Timer->PRESCALER_VAL);
+        TMR3_MODE_SELECT(_Timer);
+        TMR3_RW_CFG_SELECT(_Timer);
+        TMR3H = (_Timer->TIMER3_PRELOAD_VALUE) >> 8;
+        TMR3L = (uint8)(_Timer->TIMER3_PRELOAD_VALUE);
+        timer3_preload = _Timer->TIMER3_PRELOAD_VALUE;
+#ifdef TIMER3_INTERRUPT_FEATURE_ENABLE
+        TIMER3_INTERRUPT_ENABLE();
+        TIMER3_INTERRUPT_FLAG_CLEAR();
+        TIMER3_INTERRUPT_HANDLER = _Timer->TMR3_INTERRUPT_HANDLER;
+#if  INTERRUPT_PRIORITY_LEVELS_ENABLE==INTERRUPT_FEATURE_ENABLE
+        INTERRUPT_Priority_Level_Enable();
+        if(INTERRUPT_LOW_PRIORITY == _Timer->TMR3_priority){
+            INTERRUPT_GlobalInterruptLowEnable();
+            TIMER3_INTERRUPT_PRIORITY_LOW();
+        }
+        else if(INTERRUPT_HIGH_PRIORITY == _Timer->TMR3_priority){
+            INTERRUPT_GlobalInterruptHighEnable();
+            TIMER3_INTERRUPT_PRIORITY_HIGH();
+        }
+        else {/*Nothing*/}
+#else
+        INTERRUPT_GlobalInterruptEnable();
+        INTERRUPT_Peripheral_Interrupt_Enable(); 
+#endif
+#endif
+        TIMER3_ENABLE();
+    }
+    return ret;    
+}
+Std_ReturnType TIMER3_DENIT(const TIMER3_T *_Timer){
+    Std_ReturnType ret = E_OK;
+    if(NULL == _Timer){
+        ret = E_NOT_OK;
+    }
+    else{
+        TIMER3_DISABLE();
+#ifdef TIMER3_INTERRUPT_FEATURE_ENABLE
+        TIMER3_INTERRUPT_DISABLE();
+#endif        
+    }
+    return ret;       
+}
+Std_ReturnType TIMER3_WRITE_VALUE(const TIMER3_T *_Timer, uint16 value){
+    Std_ReturnType ret = E_OK;
+    if(NULL == _Timer){
+        ret = E_NOT_OK;
+    }
+    else{
+        TMR3H = (value) >> 8;
+        TMR3L = (uint8)(value);
+    }
+    return ret;      
+}
+Std_ReturnType TIMER3_READ_VALUE(const TIMER3_T *_Timer, uint16 *value){
+    Std_ReturnType ret = E_OK;
+    uint8 L_tmr3 = 0, H_tmr3 = 0;
+    if((NULL == _Timer) || (NULL == value)){
+        ret = E_NOT_OK;
+    }
+    else{
+        L_tmr3 = TMR3L;
+        H_tmr3 = TMR3H;
+        *value = (uint16)((H_tmr3 << 8) + L_tmr3);    
+    }
+    return ret;        
+}
+
+
+
+static inline void TMR3_MODE_SELECT(const TIMER3_T *_Timer){
+    if(TIMER3_TIMER_MODE == _Timer->TIMER3_MODE){
+        TIMER3_TIMER_MODE_ENABLE();
+    }
+    else if(TIMER3_COUNTER_MODE == _Timer->TIMER3_MODE){
+        TIMER3_COUNTER_MODE_ENABLE();
+        if(TIMER3_ASYNC_COUNTER == _Timer->TIMER3_SYNC_SELECT){
+            TIMER3_ASYNCHRONIZE_COUNTER_MODE_ENABLE();
+        }
+        else if(TIMER3_SYNC_COUNTER == _Timer->TIMER3_SYNC_SELECT){
+            TIMER3_SYNCHRONIZE_COUNTER_MODE_ENABLE();
+        }
+        else{
+            /*Nothing*/
+        }
+    }
+    else{
+        /*Nothing*/
+    }        
+}
+static inline void TMR3_RW_CFG_SELECT(const TIMER3_T *_Timer){
+    if(TIMER3_RW_16_BIT_CFG == _Timer->TIMER3_RW_MODE_CFG){
+        TIMER3_16_BIT_CONFIGURATION();
+    }
+    else if(TIMER3_RW_8_BIT_CFG == _Timer->TIMER3_RW_MODE_CFG){
+        TIMER3_8_BIT_CONFIGURATION();
+    }
+    else{
+        /*Nothing*/
+    }        
+}
+
+void TMR3_ISR(void){
+    /* The Flag of TMR0 Interrupt Must be Cleared*/
+    TIMER3_INTERRUPT_FLAG_CLEAR();
+    TMR3H = (timer3_preload) >> 8;
+    TMR3L = (uint8)(timer3_preload);
+    /*Check The Interrupt Handler Pointer To Function if it is equal to address of the Application Interrupt handler
+     And It callback the function gets called every time this ISR executes*/
+    if(TIMER3_INTERRUPT_HANDLER){
+        TIMER3_INTERRUPT_HANDLER();
+    }
+}
--- PIC18F4620_Drivers/MCAL_LAYER/Timer3/HAL_TIMER3.h
+++ PIC18F4620_Drivers/MCAL_LAYER/Timer3/HAL_TIMER3.h
@@ -9,6 +9,64 @@
 #define	HAL_TIMER3_H
 
 
+// Section: Includes
+#include "HAL_TIMER3_CFG.h"
+#include "../GPIO/HAL_GPIO.h"
+#include "pic18f4620.h"
+#include "../../MCAL_LAYER/Interrupt/MCAL_INTERNAL_INTERRUPT.h"
+// Section: Macro Declarations
+/* Timer3 On bit */
+#define TIMER3_TIMER_MODE     0
+#define TIMER3_COUNTER_MODE   1
+/* Timer3 External Clock Input Synchronization Select bit */
+#define TIMER3_ASYNC_COUNTER  1
+#define TIMER3_SYNC_COUNTER   0
+/* Timer3 Input Clock Prescale Select bits */
+#define TIMER3_PRESCALLER_DIV_1     0
+#define TIMER3_PRESCALLER_DIV_2     1
+#define TIMER3_PRESCALLER_DIV_4     2
+#define TIMER3_PRESCALLER_DIV_8     3
+/* 16-bit Read/Write Mode Enable bit */
+#define TIMER3_RW_16_BIT_CFG        1
+#define TIMER3_RW_8_BIT_CFG         0
+
+// Section: Macro Functions Declarations
+/* Timer3 On bit */
+#define TIMER3_ENABLE()                                 (T3CONbits.TMR3ON = 1)
+#define TIMER3_DISABLE()                                (T3CONbits.TMR3ON = 0)
+/* Timer3 Clock Source Select bit */
+#define TIMER3_TIMER_MODE_ENABLE()                      (T3CONbits.TMR3CS = 0)
+#define TIMER3_COUNTER_MODE_ENABLE()                    (T3CONbits.TMR3CS = 1)
+/* Timer3 External Clock Input Synchronization Select bit */
+#define TIMER3_SYNCHRONIZE_COUNTER_MODE_ENABLE()        (T3CONbits.T3SYNC = 0)
+#define TIMER3_ASYNCHRONIZE_COUNTER_MODE_ENABLE()       (T3CONbits.T3SYNC = 1)
+/* 16-bit Read/Write Mode Enable bit */
+#define TIMER3_8_BIT_CONFIGURATION()                    (T3CONbits.RD16 = 0)
+#define TIMER3_16_BIT_CONFIGURATION()                   (T3CONbits.RD16 = 1)
+/* Timer3 Input Clock Prescale Select bits */
+#define TIMER3_PRESCALLER_SELECT(_PRESCALLER_)          (T3CONbits.T3CKPS = _PRESCALLER_)
+// Section: Data Type Declarations
+typedef struct {
+#ifdef TIMER3_INTERRUPT_FEATURE_ENABLE
+    void (* TMR3_INTERRUPT_HANDLER)(void);
+    interrupt_priority_t TMR3_priority;
+#endif
+    uint16 TIMER3_PRELOAD_VALUE;
+    uint8 TIMER3_MODE : 1;
+    uint8 PRESCALER_VAL : 2;
+    uint8 TIMER3_SYNC_SELECT : 1;
+    uint8 TIMER3_RW_MODE_CFG : 1;
+    uint8 TIMER3_RESERVED : 3;
+}TIMER3_T;
+
+// Section: Function Declarations
+
+Std_ReturnType TIMER3_INIT(const TIMER3_T *_Timer);
+Std_ReturnType TIMER3_DENIT(const TIMER3_T *_Timer);
+Std_ReturnType TIMER3_WRITE_VALUE(const TIMER3_T *_Timer, uint16 value);
+Std_ReturnType TIMER3_READ_VALUE(const TIMER3_T *_Timer, uint16 *value);
+
+
 
 #endif	/* HAL_TIMER3_H */
 
